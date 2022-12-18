3261e205014cd9ef2f1650a2482c5bb266e2d7b8
gorushkin <gorushkin@gmail.com>
2022-12-19 00:44:30 +0300

    refactor: refactoring

--- src/file-manager.js
+++ src/file-manager.js
@@ -73,24 +73,6 @@ export class FileManager {
     await fs.mv(absolutePath, absoluteNewPath);
   }
 
-  async compress(itempPath, itemNewPath) {
-    if (!itempPath || !itemNewPath) throw new InputError();
-    const absolutePath = this.getAbsolutePath(itempPath);
-    const absoluteNewPath = this.getAbsolutePath(itemNewPath);
-    await fs.checkIfExist(absolutePath);
-    await fs.checkIfNotExist(absoluteNewPath);
-    await fs.compress(absolutePath, absoluteNewPath);
-  }
-
-  async decompress(itempPath, itemNewPath) {
-    if (!itempPath || !itemNewPath) throw new InputError();
-    const absolutePath = this.getAbsolutePath(itempPath);
-    const absoluteNewPath = this.getAbsolutePath(itemNewPath);
-    await fs.checkIfExist(absolutePath);
-    await fs.checkIfNotExist(absoluteNewPath);
-    await fs.decompress(absolutePath, absoluteNewPath);
-  }
-
   async cat(path) {
     if (!path) throw new InputError();
     const absolutePath = this.getAbsolutePath(path);
@@ -106,9 +88,8 @@ export class FileManager {
     await fs.checkIfExist(absolutePath);
     const item = await fs.getItem(absolutePath);
     if (!item.isFile) throw new InputError();
-    await fs.hash(absolutePath)
+    await fs.hash(absolutePath);
   }
-
 
   async add(fileName) {
     if (!fileName) throw new InputError();
@@ -177,6 +158,19 @@ export class FileManager {
     throw new InputError();
   }
 
+  async arch(direction, itempPath, itemNewPath) {
+    const mapping = {
+      compress: fs.compress,
+      decompress: fs.decompress,
+    }
+    if (!itempPath || !itemNewPath) throw new InputError();
+    const absolutePath = this.getAbsolutePath(itempPath);
+    const absoluteNewPath = this.getAbsolutePath(itemNewPath);
+    await fs.checkIfExist(absolutePath);
+    await fs.checkIfNotExist(absoluteNewPath);
+    await mapping[direction](absolutePath, absoluteNewPath);
+
+  }
 
   async command(command, arg1, arg2) {
     const commandMapping = {
@@ -192,9 +186,8 @@ export class FileManager {
       rm: this.rm.bind(this),
       os: this.os.bind(this),
       hash: this.hash.bind(this),
-      compress: this.compress.bind(this),
-      decompress: this.decompress.bind(this),
-      // compress
+      compress: this.arch.bind(this, 'compress'),
+      decompress: this.arch.bind(this, 'decompress')
     };
 
     if (commandMapping[command]) return await commandMapping[command](arg1, arg2);
